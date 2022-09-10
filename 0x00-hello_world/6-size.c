0ca3bccadf29322a038ce8afeb9d238512852266
Mkpoto <engrtony555@gmail.com>
2022-09-10 05:50:48 -0700

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,16 +9,17 @@ int main(void)
 {
 	int a;
 	long int b;
-	long long int c;
+	long long  int c;
 	char d;
 	float f;
 	
 	printf("Size of a char: %1u byte(s)\n" , (unsigned long)sizeof(d));
 	printf("Size of an int: %1u byte(s)\n" , (unsigned long)sizeof(a));
 	printf("Size of a long int: %1u byte(s)\n" , (unsigned long)sizeof(b));
-	printf("Size of a long long int: %1u byte(s)\n" i, (unsigned long)sizeof(c));
-	printf("Size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a long long int: %1u byte(s)\n" , (unsigned long)sizeof(c));
+	printof("Size of a float: %1u byte(s)\n" , (unsigned long)sizeof(f));
 	return (0);
-}}
+}
+
 
 
